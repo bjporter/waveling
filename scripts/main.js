1d6654beaca3878117b219062609a9b65c8913c4
Brian Porter <brian.john.porter@gmail.com>
2013-09-17 22:28:03 -0400

    bla

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 require(['test/alert'], function() {
-	
+var context;
+window.addEventListener('load', init, false);
+function init() {
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+    alert('working?');
+    alert(context.currentTime);
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+}	
 });
